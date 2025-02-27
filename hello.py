3932bb6f1ae2366868c8d89320cf46655388d062
Matthew <1737766+matthewgard1@users.noreply.github.com>
2025-02-26 22:00:23 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-# /// script
+#!/usr/bin/env -S uv run
 # dependencies = [
 #   "requests<3",
 #   "rich",
@@ -8,6 +8,10 @@
 import requests
 from rich.pretty import pprint
 
-resp = requests.get("https://peps.python.org/api/peps.json")
-data = resp.json()
-pprint([(k, v["title"]) for k, v in data.items()][:10])
+def main():
+    resp = requests.get("https://peps.python.org/api/peps.json")
+    data = resp.json()
+    pprint([(k, v["title"]) for k, v in data.items()][:10])
+
+if __name__ == "__main__":
+    main()
